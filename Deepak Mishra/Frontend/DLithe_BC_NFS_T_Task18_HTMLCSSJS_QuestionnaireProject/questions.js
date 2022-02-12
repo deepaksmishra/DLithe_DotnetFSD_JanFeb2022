5e2ed18c0f495e7161194ad0545e8f851f9ccd4a
Deepak Mishra <deepak123.dm98@gmail.com>
2022-02-12 16:58:28 +0530

    DLithe_BC_NFS_T_Task18_HTMLCSSJS_QuestionnaireProject
    DLithe_BC_NFS_T_Task18_HTMLCSSJS_QuestionnaireProject

---
+++ Deepak Mishra/Frontend/DLithe_BC_NFS_T_Task18_HTMLCSSJS_QuestionnaireProject/questions.js
@@ -0,0 +1,7 @@
+var questions = [
+  ["1) HTML stands for - ", "HighText Machine Language", "HyperText and links Markup Language", "HyperText Markup Language","None of these", "3"],
+  ["The correct sequence of HTML tags for starting a webpage is -", "Head, Title, HTML, body", "HTML, Body, Title, Head", " HTML, Head, Title, Body", "HTML, Head, Title, Body","4"],
+  ["Which of the following element is responsible for making the text bold in HTML?", "<pre>", "<a>", "<br>", "<b>","4"],
+  [" How many Avengers are there in the Avengers movie?", " 4", " 10", "6", "100","3"],
+  
+  ];
