5ee5cf26d51ca1fcd73a9b834286b29d20d5c6e9
Deepak Mishra <deepak123.dm98@gmail.com>
2022-02-17 23:30:13 +0530

    DLithe_BC_NFS_T_Task26_C#
    1. 1D ARRAY
    2. 2D ARRAY
    3. JAGGED ARRAY
    4. PARAMS ARRAY
    5. ARRAY METHODS

---
+++ Deepak Mishra/Frontend/c#/DLithe_BC_NFS_T_Task26_C#/array.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class ArrayExample
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };//Declaration of 1d array
+
+        //traversing array  
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
