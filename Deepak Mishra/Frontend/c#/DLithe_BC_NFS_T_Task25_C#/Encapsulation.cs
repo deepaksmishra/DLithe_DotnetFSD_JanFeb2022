1f7e4db93219a081ade9ec26199a824af728050b
Deepak Mishra <deepak123.dm98@gmail.com>
2022-02-17 23:09:29 +0530

    DLithe_BC_NFS_T_Task25_C#
    Access Specifiers
    ------------------------------
    1. Encapsulation (private, public)
    2. Inheritance (protected)
    3. 2 projects(assemblies) - internal

---
+++ Deepak Mishra/Frontend/c#/DLithe_BC_NFS_T_Task25_C#/Encapsulation.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Reflection;
+
+
+public class EncapsulationExm
+{
+
+
+    private String EmpName;
+    private int EmpAge;
+
+  
+    public String Name
+    {
+
+        get
+        {
+            return EmpName;
+        }
+
+        set
+        {
+            EmpName = value;
+        }
+
+    }
+
+    // using accessors to get and 
+    // set the value of studentAge
+    public int Age
+    {
+
+        get
+        {
+            return EmpAge;
+        }
+
+        set
+        {
+            EmpAge = value;
+        }
+
+    }
+
+
+}
+
+class Details
+{
+    static public void Main()
+    {
+
+
+
+        EncapsulationExm Employee = new EncapsulationExm();
+
+
+        Employee.Name = "Deepak";
+
+        Employee.Age = 23;
+
+        Console.WriteLine("Name: " + Employee.Name);
+        Console.WriteLine("Age: " + Employee.Age);
+
+       
+
+
+      
+    }
+}
+  
