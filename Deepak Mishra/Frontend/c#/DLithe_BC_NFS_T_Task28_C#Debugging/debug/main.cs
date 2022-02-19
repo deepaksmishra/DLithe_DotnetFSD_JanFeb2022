69dd1608d2d2a4c954b24a0044fbd004ffe6c80c
Deepak Mishra <deepak123.dm98@gmail.com>
2022-02-19 13:22:28 +0530

    DLithe_BC_NFS_T_Task28_C#Debugging

---
+++ Deepak Mishra/Frontend/c#/DLithe_BC_NFS_T_Task28_C#Debugging/debug/main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class main
+{
+    static void  Main()
+    {
+        int SNO, MARK1, MARK2, MARK3, AVERAGE , TOTAL;
+        string SNAME;
+        Console.WriteLine( "ENTER THE STUDENT DETAILS");
+        Console.WriteLine(" ENTER THE STUDENT NO");
+        SNO = int.Parse(Console.ReadLine());
+        Console.WriteLine("ENTER THE STUDENT NAME");
+        SNAME = Console.ReadLine();
+        Console.WriteLine("ENTER THE MARKS OF 3 SUBJECTS ");
+        MARK1 = int.Parse(Console.ReadLine());
+        MARK2 = int.Parse(Console.ReadLine());
+        MARK2 = int.Parse(Console.ReadLine());
+        MARK3 = int.Parse(Console.ReadLine());
+        TOTAL = MARK1 + MARK2 + MARK3;
+        AVERAGE = TOTAL;
+        Console.WriteLine("THE STUDENT DETAILS ARE GIVEN BELOW :");
+        Console.WriteLine("THE STUDENT NO IS:" + SNO);
+        Console.WriteLine("THE STUUDENT NAME IS: " + SNAME);
+        Console.WriteLine(" TOTAL MARKS IS:"+ TOTAL);
+        Console.WriteLine("AVEARGE MAARK IS:"+  AVERAGE);
+        Console.ReadKey();
+    }
+}
---
+++ Deepak Mishra/Frontend/c#/DLithe_BC_NFS_T_Task28_C#Debugging/debug02/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Program2
+{
+    public class Example
+    {
+        private int _empid, _eage;
+        private string _ename, _eaddress;
+        public int empid
+        {
+            set
+            {
+                _empid = value;
+            }
+            get
+            {
+                return _empid;
+            }
+        }
+        public int eage
+        {
+            set
+            {
+                _eage = value;
+            }
+            get
+            {
+                return _eage;
+            }
+        }
+        public string ename
+        {
+            set
+            {
+                _ename = value;
+            }
+            get
+            {
+                return _ename;
+            }
+        }
+        public string eaddress
+        {
+            set
+            {
+                _eaddress = value;
+            }
+            get
+            {
+                return _eaddress;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example obj1 = new Example();
+            obj1.empid = 101;
+            obj1.ename = "pranaya";
+            obj1.eage = 27;
+            obj1.eaddress = "bbsr";
+            Console.WriteLine("Employee details are:");
+            Console.WriteLine("employee id:" + obj1.empid);
+            Console.WriteLine("employee name:" + obj1.ename);
+            Console.WriteLine("employee age:" + obj1.eage);
+            Console.WriteLine("employee address:" + obj1.eaddress);
+            Console.ReadKey();
+        }
+    }
+}
