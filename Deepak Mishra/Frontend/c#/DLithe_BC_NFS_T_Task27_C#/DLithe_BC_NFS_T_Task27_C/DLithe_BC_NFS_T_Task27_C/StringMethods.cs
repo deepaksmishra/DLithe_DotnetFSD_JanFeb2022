1f34a024c9a6b6c943551bef818abcc9b8f1810c
Deepak Mishra <deepak123.dm98@gmail.com>
2022-02-21 17:12:35 +0530

    DLithe_BC_NFS_T_Task27_C#
    1. Strings
    2. Nullable
    3. Partial Class and Partial Method

---
+++ Deepak Mishra/Frontend/c#/DLithe_BC_NFS_T_Task27_C#/DLithe_BC_NFS_T_Task27_C/DLithe_BC_NFS_T_Task27_C/StringMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace DLithe_BC_NFS_T_Task27_C
+{
+    internal class StringMethods
+    {
+    
+
+
+        class Geeks
+        {
+
+            // Main Method
+            static void Main(string[] args)
+            {
+
+                // Creating an object array
+                // Here, It is consist of four 
+                // elements only
+                object[] array = { "Hello", "Geeks", 12345, 786 };
+
+                // Using Join method
+                // Here separator is ', '( comma )
+                string s1 = string.Join(", ", array);
+
+                // Finally after joining process gets over
+                // Getting the output of value of string s1
+                Console.WriteLine("Value of string  s1 is " + s1);
+            }
+        }
+    }
+}
+
---
+++ Deepak Mishra/Frontend/c#/DLithe_BC_NFS_T_Task27_C#/PartialClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CompleteInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Record myRecord = new Record("deepak", 15 , "nagpur");
+            myRecord.PrintRecord();
+            Console.ReadLine();
+        }
+    }
+}
