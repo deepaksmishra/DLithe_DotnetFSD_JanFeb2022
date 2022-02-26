32d4346af4e1944e1a39924f64e7b500196a5cd7
Deepak Mishra <deepak123.dm98@gmail.com>
2022-02-26 22:05:42 +0530

    Task 30

---
+++ Deepak Mishra/Frontend/c#/DLithe_BC_NFS_T_Task30/LinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLithe_BC_NFS_T_Task30
+{
+    internal class LinkedList
+    {
+       
+public class LinkedListExample
+    {
+        public static void Main(string[] args)
+        {
+            // Create a list of strings  
+            var names = new LinkedList<string>();
+            names.AddLast("Mishra");
+            names.AddLast("Deepak");
+            names.AddLast("Sam");
+            names.AddLast("Raj");
+            names.AddFirst("Mac");//added to first index  
+
+            // Iterate list element using foreach loop  
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
+}
